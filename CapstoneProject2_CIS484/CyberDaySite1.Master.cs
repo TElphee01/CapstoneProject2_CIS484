a8966fe769319394e4a81a1d57ba92ddb591a19b
TElphee01 <telphee2@gmail.com>
2020-11-05 09:26:07 -0500

    created master.

---
+++ CapstoneProject2_CIS484/CyberDaySite1.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CapstoneProject2_CIS484
+{
+    public partial class CyberDaySite1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
